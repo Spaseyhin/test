a1c94b4324188696dc4082e3f8bf321ed7e73f79
Spaseyhin <evgen.kalin99@gmail.com>
2021-01-16 15:37:04 +0400

    start

---
+++ app.rb
@@ -0,0 +1,43 @@
+#encoding: utf-8
+
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sqlite3'
+
+def init_db
+	@db = SQLite3::Database.new 'lep.db'
+	@db.results_as_hash = true	
+end
+
+before do
+	init_db
+end
+
+configure do
+	init_db
+	@db.execute 'CREATE TABLE IF NOT EXISTS "Posts"("id"INTEGER PRIMARY KEY AUTOINCREMENT, "created_date" DATE, "content" TEXT);'
+end
+get '/' do
+	@results = @db.execute 'select * from Posts order by id desc'
+	erb :index
+end
+
+get '/Newpost' do
+  erb :new
+end
+post '/new' do
+  content = params[:content]
+
+  if content.length <=0
+  	@error = 'Type poxt text'
+  	return erb :new
+  end 
+
+  @db.execute 'insert into Posts (content, created_date) values (?, datetime())', [content]
+  redirect to '/'
+
+end
+get '/som' do
+  erb ":new"
+end
